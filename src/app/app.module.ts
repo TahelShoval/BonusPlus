d7f15b8d8a74989a812a1ea95f91e55e745c12f0
TahelShoval <tahel.oshri10@gmail.com>
2021-09-01 12:22:27 +0300

    header and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule 
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeService } from 'src/app/services/home.service';
-import Worker from 'src/app/classes/worker';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- list:Worker[]=[]
-  constructor(private home:HomeService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.home.getWorkers().subscribe(res=>this.list = res)
+  }
 
-  }
-  onSave(){
-    this.home.getWorkers().subscribe(res=>this.list = res)
-  }
 }
